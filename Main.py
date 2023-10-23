4c0e20ce39f6d919a2f523e77cc2fcf16c1356cc
ChrisHesse0 <125922062+ChrisHesse0@users.noreply.github.com>
2023-10-23 17:17:04 -0400

    Add files via upload
    Template for Flask Project

---
+++ Main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+import sqlite3
+import Setup
+import random
+
+
+#Route to home page
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    return render_template('LoginScreen.html')
+
+#Route to sign up page
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    return render_template('SignUp.html')
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.close()
+    app.run()
---
+++ Setup.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+#Create database with Tables: Login, Comments, Likes, Messages
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS Login (Username TEXT, Password TEXT, First TEXT, Last TEXT, FSUID TEXT, ProfilePic INT)')
+Cursor.execute('CREATE TABLE IF NOT EXISTS Posts (Username TEXT, PostType INT, Title TEXT, Description TEXT)')
+Cursor.execute('CREATE TABLE IF NOT EXISTS Replies (PrimaryUser TEXT, SecondaryUser TEXT, Title TEXT, UserResponding TEXT, Index INT)')
+
+cursor.close()
+
+
