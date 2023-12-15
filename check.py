90f437f1f0d032e76f2fa93127f5ce37a96a1997
Blaine Timmer <bat21b@fsu.edu>
2023-12-14 20:29:44 -0500

    Completed implementing authentication token and set all routes to comply

--- check.py
+++ check.py
@@ -3,8 +3,10 @@ secret_mod = 18446744073709551615
 
 # Doesn't actually add any cryptographic complexity
 def generate_token(fsuid):
-    return int(hashlib.sha256(fsuid.encode()).hexdigest(), 16) % secret_mod
+    hashed_fsuid = int(hashlib.sha256(fsuid.encode()).hexdigest(), 16) % secret_mod
+    return str(hashed_fsuid)
 
 def check_token(token, fsuid):
     hashed_fsuid = int(hashlib.sha256(fsuid.encode()).hexdigest(), 16) % secret_mod
+    token = int(token)
     return hashed_fsuid == token
