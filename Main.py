8a303c6cd928a974f5ef96f5a1274d98e3115066
Blaine Timmer <bat21b@fsu.edu>
2023-12-16 00:07:27 -0500

    Changed the checking of authentication token so the route body is not in an if statement

--- Main.py
+++ Main.py
@@ -83,117 +83,116 @@ def welcome():
 def main():
     session_token = request.cookies.get('auth_token')
 
-    if check_token(session_token, user[0]):
-        conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Posts ORDER BY Id DESC")
-        posts = cur.fetchall()
-
-        return render_template('MainPage.html', posts=posts)
-    else:
+    if not check_token(session_token, user[0]):
         return render_template('TokenError.html')
+
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Posts ORDER BY Id DESC")
+    posts = cur.fetchall()
+
+    return render_template('MainPage.html', posts=posts)
 
 @app.route('/addpost', methods = ['POST', 'GET'])
 def addpost():
     session_token = request.cookies.get('auth_token')
 
-    if check_token(session_token, user[0]):
-        return render_template('AddPost.html')
-    else:
+    if not check_token(session_token, user[0]):
         return render_template('TokenError.html')
+
+    return render_template('AddPost.html')
 
 @app.route('/added', methods = ['POST', 'GET'])
 def added():
     if request.method == "POST":
         session_token = request.cookies.get('auth_token')
 
-        if check_token(session_token, user[0]):
-            con = sqlite3.connect('database.db')
-            try:
-                title = request.form['Title']
-                description = request.form['Description']
-
-                with sqlite3.connect('database.db') as con:
-                    cur = con.cursor()
-                    cur.execute("INSERT INTO Posts (FSUID, Title,Description) VALUES (?,?,?)", (user[0], title, description))
-            except:
-                con.rollback()
-                return render_template('Error.html')
-            finally:
-                con.close()
-            
-            return redirect("/main")
-        else:
+        if not check_token(session_token, user[0]):
             return render_template('TokenError.html')
+        
+        con = sqlite3.connect('database.db')
+        try:
+            title = request.form['Title']
+            description = request.form['Description']
+
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO Posts (FSUID, Title,Description) VALUES (?,?,?)", (user[0], title, description))
+        except:
+            con.rollback()
+            return render_template('Error.html')
+        finally:
+            con.close()
+        
+        return redirect("/main")
 
 @app.route('/post_detail/<int:post_id>')
 def post_detail(post_id):
     session_token = request.cookies.get('auth_token')
 
-    if check_token(session_token, user[0]):
-        conn = sqlite3.connect("database.db")
-        cur = conn.cursor()
-
-        cur.execute("SELECT * FROM Posts WHERE Id = ?", (post_id,))
-        post = cur.fetchall()
-
-        authorFSUID = post[0][1]
-        title = post[0][2]
-        description = post[0][3]
-
-        cur.execute("SELECT Author, Content FROM Comments WHERE PostId = ? ORDER BY CommentId ASC", (post_id,))
-        comments = cur.fetchall()
-
-        return render_template('PostDetail.html', post_id=post_id, title=title, description=description, fsuid=authorFSUID, comments=comments)
-    else:
+    if not check_token(session_token, user[0]):
         return render_template('TokenError.html')
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM Posts WHERE Id = ?", (post_id,))
+    post = cur.fetchall()
+
+    authorFSUID = post[0][1]
+    title = post[0][2]
+    description = post[0][3]
+
+    cur.execute("SELECT Author, Content FROM Comments WHERE PostId = ? ORDER BY CommentId ASC", (post_id,))
+    comments = cur.fetchall()
+
+    return render_template('PostDetail.html', post_id=post_id, title=title, description=description, fsuid=authorFSUID, comments=comments)
 
 @app.route('/messages', methods = ['POST', 'GET'])
 def messages():
     session_token = request.cookies.get('auth_token')
 
-    if check_token(session_token, user[0]):
-        conn = sqlite3.connect("database.db")
-        cur = conn.cursor()
-
-        cur.execute("SELECT PrimaryUser, SecondaryUser FROM Replies WHERE PrimaryUser = ? OR SecondaryUser = ? ORDER BY Ind DESC", (user[0], user[0]))
-        replies = cur.fetchall()
-
-        fsuids = set()
-        for reply in replies:
-            if reply[0] != user[0]:
-                fsuids.add(reply[0])
-            else:
-                fsuids.add(reply[1])
-
-        query = "SELECT FSUID, First FROM Login WHERE FSUID IN ({})".format(', '.join('?' for _ in fsuids))
-        cur.execute(query, tuple(fsuids))
-
-        results = cur.fetchall()
-
-        return render_template("Messages.html", replies=results)
-    else:
+    if not check_token(session_token, user[0]):
         return render_template('TokenError.html')
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT PrimaryUser, SecondaryUser FROM Replies WHERE PrimaryUser = ? OR SecondaryUser = ? ORDER BY Ind DESC", (user[0], user[0]))
+    replies = cur.fetchall()
+
+    fsuids = set()
+    for reply in replies:
+        if reply[0] != user[0]:
+            fsuids.add(reply[0])
+        else:
+            fsuids.add(reply[1])
+
+    query = "SELECT FSUID, First FROM Login WHERE FSUID IN ({})".format(', '.join('?' for _ in fsuids))
+    cur.execute(query, tuple(fsuids))
+
+    results = cur.fetchall()
+
+    return render_template("Messages.html", replies=results)
 
 @app.route('/sendmessages', methods = ['POST', 'GET'])
 def sendmessages():
     if request.method == 'POST':
         session_token = request.cookies.get('auth_token')
 
-        if check_token(session_token, user[0]):
-            conn = sqlite3.connect('database.db')
-            try:
-                other_person = request.form["fsuid"]
-                cur = conn.cursor()
-
-                cur.execute("SELECT PrimaryUser, Message FROM Replies WHERE (PrimaryUser = ? AND SecondaryUser = ?) OR (PrimaryUser = ? AND SecondaryUser = ?) ORDER BY Ind ASC", (user[0], other_person, other_person, user[0]))
-                authors_and_messages = cur.fetchall()
-
-                return render_template("SendMessage.html", authors_and_messages=authors_and_messages, other=other_person)
-            except:
-                return render_template('Error.html')
-        else:
+        if not check_token(session_token, user[0]):
             return render_template('TokenError.html')
+        conn = sqlite3.connect('database.db')
+        try:
+            other_person = request.form["fsuid"]
+            cur = conn.cursor()
+
+            cur.execute("SELECT PrimaryUser, Message FROM Replies WHERE (PrimaryUser = ? AND SecondaryUser = ?) OR (PrimaryUser = ? AND SecondaryUser = ?) ORDER BY Ind ASC", (user[0], other_person, other_person, user[0]))
+            authors_and_messages = cur.fetchall()
+
+            return render_template("SendMessage.html", authors_and_messages=authors_and_messages, other=other_person)
+        except:
+            return render_template('Error.html')
 
 
 @app.route('/send', methods = ['POST','GET'])
@@ -201,79 +200,79 @@ def sendMessages():
     if request.method == 'POST':
         session_token = request.cookies.get('auth_token')
 
-        if check_token(session_token, user[0]):
-            conn = sqlite3.connect('database.db')
-            try:
-                other = request.form["other"]
-                message = request.form["message"]
-                cur = conn.cursor()
-                cur.execute("INSERT INTO Replies (PrimaryUser,SecondaryUser,Message) VALUES (?,?,?)", (user[0], other, message))
-                conn.commit()
-                cur.execute("SELECT PrimaryUser, Message FROM Replies WHERE (PrimaryUser = ? AND SecondaryUser = ?) OR (PrimaryUser = ? AND SecondaryUser = ?) ORDER BY Ind ASC", (user[0], other, other, user[0]))
-                authors_and_messages = cur.fetchall()
-
-                return render_template("SendMessage.html", authors_and_messages=authors_and_messages, other=other)
-
-            except:
-                conn.rollback()
-                return render_template('Error.html')
-            finally:
-                conn.close()
-        else:
+        if not check_token(session_token, user[0]):
             return render_template('TokenError.html')
+
+        conn = sqlite3.connect('database.db')
+        try:
+            other = request.form["other"]
+            message = request.form["message"]
+            cur = conn.cursor()
+            cur.execute("INSERT INTO Replies (PrimaryUser,SecondaryUser,Message) VALUES (?,?,?)", (user[0], other, message))
+            conn.commit()
+            cur.execute("SELECT PrimaryUser, Message FROM Replies WHERE (PrimaryUser = ? AND SecondaryUser = ?) OR (PrimaryUser = ? AND SecondaryUser = ?) ORDER BY Ind ASC", (user[0], other, other, user[0]))
+            authors_and_messages = cur.fetchall()
+
+            return render_template("SendMessage.html", authors_and_messages=authors_and_messages, other=other)
+
+        except:
+            conn.rollback()
+            return render_template('Error.html')
+        finally:
+            conn.close()
 
 @app.route("/search", methods = ["POST", "GET"])
 def searched():
     if request.method == "POST":
         session_token = request.cookies.get('auth_token')
 
-        if check_token(session_token, user[0]):
-            conn = sqlite3.connect('database.db')
-            try:
-                searched = request.form["searched"]
-                cur = conn.cursor()
-                query = """
-                    SELECT * FROM Posts
-                    WHERE FSUID LIKE ? COLLATE NOCASE
-                    OR Title LIKE ? COLLATE NOCASE
-                    OR Description LIKE ? COLLATE NOCASE
-                    ORDER BY Id DESC"""
-
-                cur.execute(query, ('%' + searched + '%', '%' + searched + '%', '%' + searched + '%'))
-
-                posts = cur.fetchall()
-                return render_template('MainPage.html', posts=posts)
-
-            except:
-                conn.rollback()
-                return render_template('Error.html')
-            finally:
-                conn.close()
-        else:
+        if not check_token(session_token, user[0]):
             return render_template('TokenError.html')
+        
+        conn = sqlite3.connect('database.db')
+        try:
+            searched = request.form["searched"]
+            cur = conn.cursor()
+            query = """
+                SELECT * FROM Posts
+                WHERE FSUID LIKE ? COLLATE NOCASE
+                OR Title LIKE ? COLLATE NOCASE
+                OR Description LIKE ? COLLATE NOCASE
+                ORDER BY Id DESC"""
+
+            cur.execute(query, ('%' + searched + '%', '%' + searched + '%', '%' + searched + '%'))
+
+            posts = cur.fetchall()
+            return render_template('MainPage.html', posts=posts)
+
+        except:
+            conn.rollback()
+            return render_template('Error.html')
+        finally:
+            conn.close()
 
 @app.route("/comment", methods = ["POST", "GET"])
 def comment():
     if request.method == "POST":
         session_token = request.cookies.get('auth_token')
 
-        if check_token(session_token, user[0]):
-            conn = sqlite3.connect('database.db')
-            try:
-                post_id = request.form["post_id"]
-                content = request.form["content"]
-                cur = conn.cursor()
-                cur.execute("INSERT INTO Comments (PostId,Content,Author) VALUES (?,?,?)", (post_id, content, user[0]))
-                conn.commit()
-                return redirect('/post_detail/' + post_id)
-
-            except:
-                conn.rollback()
-                return render_template('Error.html')
-            finally:
-                conn.close()
-        else:
+        if not check_token(session_token, user[0]):
             return render_template('TokenError.html')
+
+        conn = sqlite3.connect('database.db')
+        try:
+            post_id = request.form["post_id"]
+            content = request.form["content"]
+            cur = conn.cursor()
+            cur.execute("INSERT INTO Comments (PostId,Content,Author) VALUES (?,?,?)", (post_id, content, user[0]))
+            conn.commit()
+            return redirect('/post_detail/' + post_id)
+
+        except:
+            conn.rollback()
+            return render_template('Error.html')
+        finally:
+            conn.close()
 
 
 if __name__ == "__main__":
